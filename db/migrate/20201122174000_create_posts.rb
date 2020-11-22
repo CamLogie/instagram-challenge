d525c56e98afc02b99cf14a066486c0e71140ea6
Cameron Logie <cameronlogie24@gmail.com>
2020-11-22 18:02:58 +0000

    tried to add avatar to db

--- db/migrate/20201122174000_create_posts.rb
+++ db/migrate/20201122174000_create_posts.rb
@@ -1,7 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
-    create_table :posts do |t|
-
+    change_table :posts do |t|
+      t.string :message
+      t.attachment :avatar
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_174000) do
+ActiveRecord::Schema.define(version: 2020_11_22_175047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "username"
+    t.string "password"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.bigint "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
