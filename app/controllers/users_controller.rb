194fd0e58efe660bec3003f7ea888f1403c81efc
Cameron Logie <cameronlogie24@gmail.com>
2020-11-23 09:21:24 +0000

    new user signup available

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+
+    User.create(username: params["username"], email: params['email'], password: params['password'])
+
+  end
+
 end
